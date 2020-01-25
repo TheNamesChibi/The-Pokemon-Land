152ddcf234f12472e17630a7df2824c8f003ea8c
Star of Aquarius <crystalfranks18@hotmail.com>
2020-01-25 07:58:12 -0500

    updates

--- config/formats.js
+++ config/formats.js
@@ -2251,24 +2251,7 @@ let Formats = [
 			this.add('raw|SUPER CAST BROS!!');
 			this.add('message', 'GET READY FOR BATTLE!');
             this.add(`raw|<div class='broadcast-red'><b>Got to know what the custom moves, items and abilities do?<br />Use <code>/details [move/item/ability name]</code> to find out!</b></div>`);
-            this.add(`raw|<div class='broadcast-blue'><b>You can't Dynamax if your team has a Mega Stone or a Z-Crystal!</b></div>`);
         if (this.teamGenerator.allXfix) this.add(`c|~KrisTami|Holy smokes! I left the xfix sets at DeviantART by an accident!`);
-        	// if you have a mega or z, you can't dynamax
-			for (const side of this.sides) {
-				let canMegaOrZ = false;
-				for (const pokemon of side.pokemon) {
-					const item = this.dex.getItem(pokemon.item);
-					if (item.megaStone || item.zMove) {
-						canMegaOrZ = true;
-						break;
-					}
-				}
-				if (canMegaOrZ) {
-					for (const pokemon of side.pokemon) {
-						pokemon.canDynamax = false;
-					}
-				}
-			}
 		},
 		onSwitchInPriority: 100,
 		onSwitchIn(pokemon) {
@@ -2300,22 +2283,6 @@ let Formats = [
 			this.add('raw|<h1>Super Free-For-All!</h1>');
 			this.add('raw|<h2>Let\'s Go!</h2>');
 			this.trunc = n => Math.trunc(n);
-			// if you have a mega or z, you can't dynamax
-			for (const side of this.sides) {
-				let canMegaOrZ = false;
-				for (const pokemon of side.pokemon) {
-					const item = this.dex.getItem(pokemon.item);
-					if (item.megaStone || item.zMove) {
-						canMegaOrZ = true;
-						break;
-					}
-				}
-				if (canMegaOrZ) {
-					for (const pokemon of side.pokemon) {
-						pokemon.canDynamax = false;
-					}
-				}
-			}
 		},
 		onSwitchInPriority: 100,
 		onSwitchIn(pokemon) {
